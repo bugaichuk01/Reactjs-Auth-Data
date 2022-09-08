311feecb74416716d7f872539f29770350ab77c5
bugaichuk01 <bugaichuk001@gmail.com>
2022-09-08 18:43:50 +0300

    Refactor index file

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createGlobalStyle, ThemeProvider} from "styled-components";
+import {setupStore} from './store/store';
+import {Provider} from "react-redux";
+
+const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Inter;
+  }`;
+
+const theme = {
+    phone: '(max-width: 425px)',
+    desktop: '(max-width: 1920px) and (min-width: 1025px)'
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    document.getElementById('root') as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const store = setupStore();
+
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <ThemeProvider theme={theme}>
+                <Global/>
+                <App/>
+            </ThemeProvider>
+        </Provider>
+    </React.StrictMode>
+);
